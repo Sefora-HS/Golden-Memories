5709a2b63c3490cfa37a24b3eb3d4d9c6e13e33b
Sefora-HS <sefora.sylla@hotmail.com>
2026-03-02 21:42:26 +0100

    boxicons

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/app.css">
     <title>Golden Memories</title>
 </head>
 <body>
+    <nav>
+        <ul>
+            <li></li>
+        </ul>
+    </nav>
+
+    <!-- boxicons -->
+    <script src="https://unpkg.com/@boxicons/js@latest"></script>
+    <script>
+    // When using CDN, all icons are available
+    boxicons.getIcons({ icons: boxicons.icons });
+    </script>
+    
 </body>
 </html>
