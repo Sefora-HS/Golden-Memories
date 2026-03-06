f19a4978f49a94fd14f3eaa1aa0bb5113a11a672
Sefora-HS <sefora.sylla@hotmail.com>
2026-03-06 20:24:09 +0100

    lien config

--- vue/pages/templates/navbar.php
+++ vue/pages/templates/navbar.php
@@ -1,44 +1,40 @@
 <?php
+include_once __DIR__ . '/../../../modele/config.php';
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <nav class="navigation">
 <ul>
 
-<!-- albums -->
 <li class="list <?= ($current_page == 'index.php') ? 'active' : '' ?>">
-    <a href="<?= ($current_page == 'index.php') ? '#' : 'index.php' ?>">
+    <a href="<?= ($current_page == 'index.php') ? '#' : BASE_URL . 'index.php' ?>">
         <span class="icon"><ion-icon name="images-outline"></ion-icon></span>
         <p>Albums</p>
     </a>
 </li>
 
-<!-- explorer -->
 <li class="list <?= ($current_page == 'explore.php') ? 'active' : '' ?>">
-    <a href="<?= ($current_page == 'explore.php') ? '#' : './vue/pages/explore.php' ?>">
+    <a href="<?= ($current_page == 'explore.php') ? '#' : BASE_URL . 'vue/pages/explore.php' ?>">
         <span class="icon"><ion-icon name="shuffle-outline"></ion-icon></span>
         <p>Explorer</p>
     </a>
 </li>
 
-<!-- ajouter -->
 <li class="list">
     <a href="#">
         <span class="icon"><ion-icon name="add-circle" class="circle"></ion-icon></span>
     </a>
 </li>
 
-<!-- partages -->
 <li class="list <?= ($current_page == 'partages.php') ? 'active' : '' ?>">
-    <a href="<?= ($current_page == 'partages.php') ? '#' : 'partages.php' ?>">
+    <a href="<?= ($current_page == 'partages.php') ? '#' : BASE_URL . 'partages.php' ?>">
         <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
         <p>Partages</p>
     </a>
 </li>
 
-<!-- capsules -->
 <li class="list <?= ($current_page == 'capsules.php') ? 'active' : '' ?>">
-    <a href="<?= ($current_page == 'capsules.php') ? '#' : 'capsules.php' ?>">
+    <a href="<?= ($current_page == 'capsules.php') ? '#' : BASE_URL . 'capsules.php' ?>">
         <span class="icon"><ion-icon name="time-outline"></ion-icon></span>
         <p>Capsules</p>
     </a>
