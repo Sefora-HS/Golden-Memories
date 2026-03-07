a06b04da40ed0507af8db2076d27bcda6f9ef399
Sefora-HS <sefora.sylla@hotmail.com>
2026-03-07 20:32:09 +0100

    taille texte note

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $souvenirs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./vue/assets/css/app.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/vue/assets/css/app.css?v=<?= time() ?>">
     <title>Golden Memories</title>
 </head>
 <body>
